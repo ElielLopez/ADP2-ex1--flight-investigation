0d2ae24d8a0e77818aa5211440b06e7ab9bc5102
ElielLopez <elielsw1@gmail.com>
2021-04-05 17:29:36 +0300

    Add project files.

---
+++ ADP2/Models/VirtualJoystickEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADP2.Models
+{
+    public class VirtualJoystickEventArgs
+    {
+        public double Aileron
+        {
+            get;
+            set;
+        }
+        public double Elevator
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ ADP2/Views/OpenFile.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace ADP2.Views
+{
+    /// <summary>
+    /// Interaction logic for OpenFile.xaml
+    /// </summary>
+    public partial class OpenFile : UserControl
+    {
+        public OpenFile()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".csv";
+            //dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string choosenFileName = dlg.FileName;
+                fileNameTextBox.Text = choosenFileName;
+                //openFileButton.Text = filename;
+            }
+
+            /*            if (result == true)
+                        {
+                            System.IO.StreamReader sr = new
+                            System.IO.StreamReader(dlg.FileName);
+                            MessageBox.Show(sr.ReadToEnd());
+                            sr.Close();
+                        }*/
+        }
+    }
+}
