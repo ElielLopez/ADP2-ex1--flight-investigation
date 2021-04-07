d0fc7b6e897594d357f24b11a7b93f048f83f1ad
ElielLopez <elielsw1@gmail.com>
2021-04-07 20:23:48 +0300

    Combining to 1 view

--- ADP2/MainWindow.xaml.cs
+++ ADP2/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace ADP2
 {
@@ -23,6 +27,90 @@ namespace ADP2
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            Mutex m = new Mutex();
+            int lineNumber = 0;
+            try
+            {
+                TcpClient client = new TcpClient("127.0.0.1", 5400);
+                StreamWriter stream = new StreamWriter(client.GetStream());
+                var lines = File.ReadLines("reg_flight.csv"); //TODO
+                //this._num_line = 0;
+
+                string l = lines.ElementAt(lineNumber);
+                while (l != null)
+                {
+                    //Byte[] data = System.Text.Encoding.ASCII.GetBytes(l);
+                    stream.Write(l, 0, l.Length);
+                    Thread.Sleep(1000 / 10);
+                    m.WaitOne();
+                    lineNumber++;
+                    m.ReleaseMutex();
+                    l = lines.ElementAt(lineNumber);
+                }
+
+                stream.Close();
+
+            }
+            catch
+            {
+
+            }
+
+            //------------
+
+
+            /*            string line;
+                        int temp = 0;
+                        StreamReader file = new StreamReader("reg_flight.csv");
+                        List<string> listOfNames = new List<string>();
+                        //List<string> listOfNamesFinal = new List<string>();
+                        while ((line = file.ReadLine()) != null)
+                        {
+                            char[] seps = new char[] { ',' };
+                            string[] parts = line.Split(seps, StringSplitOptions.None);
+                            for (int i = 0; i < parts.Length; i++)
+                            {
+                                i++;
+                                temp++;
+                                listOfNames.Add(parts[i]);
+                            }
+                        }
+                        file.Close();*/
+        }
+
+        private void OpenXMLFile_Click(object sender, RoutedEventArgs e)
+        {
+            string line;
+            int temp = 0;
+            StreamReader file = new StreamReader("playback_small.xml");
+            List<string> listOfNames = new List<string>();
+            List<string> listOfNamesFinal = new List<string>();
+            while ((line = file.ReadLine()) != null)
+            {
+                char[] seps = new char[] { '<', '>' };
+                string[] parts = line.Split(seps, StringSplitOptions.None);
+                for (int i = 0; i < parts.Length; i++)
+                {
+                    if (parts[i] == "name")
+                    {
+                        i++;
+                        temp++;
+                        listOfNames.Add(parts[i]);
+                    }
+                    if (parts[i] == "input")
+                        break;
+                }
+            }
+            file.Close();
+            for (int i = 0; i < temp / 2; i++)
+            {
+                listOfNamesFinal.Add(listOfNames[i]);
+                Console.WriteLine(listOfNamesFinal[i]);
+            }
         }
     }
 }
