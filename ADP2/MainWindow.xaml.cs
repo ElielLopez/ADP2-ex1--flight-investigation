06244eb909fcc91b178ca96eded354c9d8257b3c
ElielLopez <elielsw1@gmail.com>
2021-04-09 22:24:04 +0300

    csv file saving

--- ADP2/MainWindow.xaml.cs
+++ ADP2/MainWindow.xaml.cs
@@ -29,58 +29,99 @@ namespace ADP2
             InitializeComponent();
         }
 
+        int sleepTime = 100;
+        int counter = 0;
+        int lineNumber = 1;
+
         private void OpenFile_Click(object sender, RoutedEventArgs e)
         {
-            Mutex m = new Mutex();
-            int lineNumber = 0;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+
+            Nullable<bool> result = openFileDialog.ShowDialog();
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                string filename = openFileDialog.FileName;
+                fileNameTextBox.Text = filename;
+            }
+
+            using (StreamReader file = new StreamReader(fileNameTextBox.Text))
+            {
+                counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    counter++;
+                }
+                file.Close();
+                Console.WriteLine("NUMBER OF LINES IS {0}", counter);
+            }
+
+
+            string[] arrText;
+            string lineThreeHundred;
+
+            arrText = File.ReadAllLines(fileNameTextBox.Text);
+            //lineThreeHundred = arrText[counter];
+
+            //Mutex m = new Mutex();
+            //int lineNumber = 0;
             try
             {
                 TcpClient client = new TcpClient("127.0.0.1", 5400);
-                StreamWriter stream = new StreamWriter(client.GetStream());
-                var lines = File.ReadLines("reg_flight.csv"); //TODO
-                //this._num_line = 0;
+                NetworkStream stream = client.GetStream();
+                //StreamWriter stream = new StreamWriter(client.GetStream());
+                var lines = File.ReadLines(fileNameTextBox.Text); //TODO
+                                                                  //this._num_line = 0;
 
-                string l = lines.ElementAt(lineNumber);
-                while (l != null)
+                //string l = lines.ElementAt(lineNumber);
+                //while(l != null)
+
+                /*                foreach (string l in lines)
+                                {
+                                    Byte[] data = System.Text.Encoding.ASCII.GetBytes(l + "\r\n");
+                                    stream.Write(data, 0, data.Length);
+                                    Thread.Sleep(sleepTime);
+                                    //m.WaitOne();
+                                    //lineNumber++;
+                                    //m.ReleaseMutex();
+                                    //l = lines.ElementAt(lineNumber);
+                                }*/
+
+                // int x = Int32.Parse(str);
+                for (int i = 1; i < counter; i++)
                 {
-                    //Byte[] data = System.Text.Encoding.ASCII.GetBytes(l);
-                    stream.Write(l, 0, l.Length);
-                    Thread.Sleep(1000 / 10);
-                    m.WaitOne();
-                    lineNumber++;
-                    m.ReleaseMutex();
-                    l = lines.ElementAt(lineNumber);
+                    lineNumber = (int)timeSlider.Value;
+                    //lineNumber *= 10;
+                    Console.WriteLine(lineNumber);
+                    i = i + lineNumber;
+                    lineThreeHundred = arrText[i];
+                    Byte[] data = System.Text.Encoding.ASCII.GetBytes(lineThreeHundred + "\r\n");
+                    stream.Write(data, 0, data.Length);
+                    Thread.Sleep(sleepTime);
                 }
 
+
+
                 stream.Close();
+                client.Close();
 
             }
-            catch
+            catch (Exception _e)
             {
-
+                Console.WriteLine("error");
             }
 
-            //------------
 
-
-            /*            string line;
-                        int temp = 0;
-                        StreamReader file = new StreamReader("reg_flight.csv");
-                        List<string> listOfNames = new List<string>();
-                        //List<string> listOfNamesFinal = new List<string>();
-                        while ((line = file.ReadLine()) != null)
-                        {
-                            char[] seps = new char[] { ',' };
-                            string[] parts = line.Split(seps, StringSplitOptions.None);
-                            for (int i = 0; i < parts.Length; i++)
-                            {
-                                i++;
-                                temp++;
-                                listOfNames.Add(parts[i]);
-                            }
-                        }
-                        file.Close();*/
         }
+
+
+
 
         private void OpenXMLFile_Click(object sender, RoutedEventArgs e)
         {
